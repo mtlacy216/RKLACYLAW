170319bdf8f83ee5828bd85d0d90d70b6d3e258e
Michael Lacy <michaeltlacy@gmail.com>
2025-06-13 02:00:01 +0000

    Auto backup 2025-06-13

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+env: {
+  N8N_PORT: 5680,
+  N8N_HOST: "n8n.intelechia.com",
+  N8N_PROTOCOL: "https",
+  N8N_LOG_LEVEL: "info",
+
+  N8N_DATA_FOLDER: "/mnt/n8n-db/20250612-data",
+  N8N_USER_FOLDER: "/mnt/n8n-db/20250612-config",
+
+  N8N_ENCRYPTION_KEY: "9e34094a0f7241eab69bd81b56d5b0a3b91e834faed33eaa142fda28ad74a3d4",
+  N8N_AUTH_MODE: "db"
+}
+
